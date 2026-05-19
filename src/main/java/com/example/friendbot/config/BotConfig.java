a10392a5ec5d9f0f82fd134dae743715b66e90c0
Edward <pakachun48@gmail.com>
2026-05-19 18:46:10 +0300

    feat: add @Validated

--- src/main/java/com/example/friendbot/config/BotConfig.java
+++ src/main/java/com/example/friendbot/config/BotConfig.java
@@ -4,11 +4,13 @@ import lombok.Getter;
 import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.validation.annotation.Validated;
 
 @Configuration
 @ConfigurationProperties(prefix = "bot")
 @Getter
 @Setter
+@Validated
 public class BotConfig {
     private String token;
     private String username;
